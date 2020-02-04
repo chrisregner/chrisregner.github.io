f36bedead0b818b2777c082262540c935ca87792
Christopher Regner <dev@chrisregner.com>
2020-02-04 18:42:50 +0800

    Add minor copy update

--- src/components/WorksSection/WorksSection.js
+++ src/components/WorksSection/WorksSection.js
@@ -7,7 +7,7 @@ const WorksSection = () => {
 
   return (
     <article className='center mh4 mb5 mw7'>
-      <h2 className='f4 mb4 pb3 normal ttu tracked tc'>Sample Projects</h2>
+      <h2 className='f4 mb4 pb3 normal ttu tracked tc'>Sample Works</h2>
 
       <ul className='list pa0'>
         {data.site.siteMetadata.works.map((w, i) =>
