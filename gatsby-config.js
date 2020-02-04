e4cfa047ddebacd9bef7b0b449f9e04e5362d956
Christopher Regner <dev@chrisregner.com>
2020-02-04 18:42:08 +0800

    Add a gallery type work

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,47 @@ module.exports = {
   siteMetadata: {
     works: [
       {
-        type: 'basic',
+        type: 'gallery',
         title: 'Hey You',
         tags: ['employment work', 'react.js', 'redux', 'e-payment'],
         description: 'Single-page app that allows users to search venues in Australia and order ahead. Users pay through e-payment system and will be updated on the order status page (accepted, rejected, or ready for pick up). This saves users from spending time on queues as they instead can come to venues once the order is ready.',
         source: null,
         demo: 'https://heyyou.com.au/',
-        img: {
-          url: '/img/hey-you.png',
-          alt: 'Hey You',
-        },
+        img: null,
+        imgs: [
+          {
+            url: '/img/hey-you-1.png',
+            alt: 'Homepage',
+          },
+          {
+            url: '/img/hey-you-2.png',
+            alt: 'Mobile Screens',
+          },
+          {
+            url: '/img/hey-you-3.png',
+            alt: 'Search Suggestions Overlay',
+          },
+          {
+            url: '/img/hey-you-4.png',
+            alt: 'Search Page',
+          },
+          {
+            url: '/img/hey-you-5.png',
+            alt: 'Venue Menu Page',
+          },
+          {
+            url: '/img/hey-you-6.png',
+            alt: 'Venue Menu Options Modal',
+          },
+          {
+            url: '/img/hey-you-7.png',
+            alt: 'Payment Input Modal',
+          },
+          {
+            url: '/img/hey-you-8.png',
+            alt: 'Payment Page',
+          },
+        ],
       },
       {
         type: 'negative-margin',
@@ -20,6 +51,7 @@ module.exports = {
         description: 'A web app that is being displayed from a mobile app. Lists lotto winners, shows their details, and sorts them by date or amount.',
         source: null,
         demo: null,
+        imgs: [],
         img: {
           url: '/img/french-lotto-winners.png',
           alt: 'Lotto Winners Listing App (Anonymous)',
@@ -32,6 +64,7 @@ module.exports = {
         description: 'App where you can search github repositories and compare their stats, such as stars and issues count, with charts (Bar, Pie, Line chart).',
         source: 'https://github.com/chrisregner/github-compare',
         demo: 'https://chrisregner.github.io/github-compare',
+        imgs: [],
         img: {
           url: '/img/github-compare.png',
           alt: 'Github Compare',
@@ -44,6 +77,7 @@ module.exports = {
         description: 'A React/Redux website that gets its data from WordPress through its REST API - the means by which developers can create Single-Page Applications (SPA) with WordPress. Just like most CMS, it retrieves posts by list or individually. It also allows you to filter posts by keyword or tags.',
         source: 'https://github.com/chrisregner/minimal-react-wordpress',
         demo: 'https://chrisregner.github.io/minimal-react-wordpress',
+        imgs: [],
         img: {
           url: '/img/minimal-react-wordpress.png',
           alt: 'Minimal React WordPress',
@@ -56,6 +90,7 @@ module.exports = {
         description: 'A web app that functions like a travel journal that allows users to make entries about the places they have been, while also doubling as planner where they can create plans and schedules. These two main features are also visualized with a map (using Google Map).        ',
         source: 'https://github.com/chrisregner/plans-and-journals',
         demo: 'https://chrisregner.github.io/plans-and-journals',
+        imgs: [],
         img: {
           url: 'https://chrisregner.github.io/img/plans-and-journals.png',
           alt: 'Plans and Journals',
@@ -68,6 +103,7 @@ module.exports = {
         description: 'A simple web app where users can manage a list of routines/tasks and track the time being spent on each until the set goal is reached.',
         source: 'https://github.com/chrisregner/daily-routines-tracker',
         demo: 'https://chrisregner.github.io/daily-routines-tracker',
+        imgs: [],
         img: {
           url: 'https://chrisregner.github.io/img/daily-routines-tracker.png',
           alt: 'Daily Routines Tracker',
--- src/components/WorksSection/WorksSection.js
+++ src/components/WorksSection/WorksSection.js
@@ -34,6 +34,10 @@ const query = graphql`
             url
             alt
           }
+          imgs {
+            url
+            alt
+          }
         }
       }
     }
